4a35cbb413a525b8ff00407b4e5cf030d0ee92db
volodia <shonkatler@gmail.com>
2024-05-28 11:10:03 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        convertSgtins();
+    }
+
+    public static void convertSgtins(){
+        File file = new File("src/main/java/org/example/22122.txt");
+        List<String> gtins = new ArrayList<>();
+        List<String> last3chars = new ArrayList<>();
+        try (Reader reader = new FileReader(file)) {
+            BufferedReader br = new LineNumberReader(reader);
+            while (br.ready()){
+                String rawLine = br.readLine();
+                if (rawLine.startsWith("01")){
+                    System.out.println(rawLine.indexOf("01"));
+                    rawLine = rawLine.substring(2, 33);
+                    System.out.println(rawLine);
+                }
+                System.out.println(rawLine);
+                System.out.println(rawLine.substring(14,16));
+                String sgtin = rawLine.substring(0, 14) + rawLine.substring(16, 29);
+                last3chars.add(sgtin.substring(24, 27));
+                gtins.add(sgtin);
+            }
+            br.close();
+            File file1 = new File("new.txt");
+            Writer writer = new FileWriter(file1);
+            String prev = "";
+            for (String s : gtins){
+                writer.write(s + '\n');
+            }
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
